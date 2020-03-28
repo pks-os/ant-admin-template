20dbfa69105ee3b3bd3d2ec8bca56cd04042afdd
Tushar Saxena <tushar.saxena@outlook.com>
2020-03-29 01:33:30 +0530

    Initial Commit

---
+++ src/main/java/com/antlab/admin/AntlabAdminTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.antlab.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntlabAdminTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntlabAdminTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antlab/admin/AntlabAdminTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antlab.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntlabAdminTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
